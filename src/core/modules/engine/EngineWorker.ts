1b1806dc1161023adad045d69b59f7527c8175f9
Ruslan Gadeev <rgadeew@yandex.ru>
2025-09-20 23:12:02 +0300

    fix internal engine lag

--- src/core/modules/engine/EngineWorker.ts
+++ src/core/modules/engine/EngineWorker.ts
@@ -2,10 +2,15 @@ import Worker from 'web-worker';
 import EngineProcess from './EngineProcess.ts';
 
 class EngineWorker extends EngineProcess {
-  private worker: Worker;
+  private worker!: Worker;
+  private options: Record<string, string> = {};
 
   constructor() {
     super();
+    this.useNextWorker();
+  }
+
+  private useNextWorker() {
     const url = new URL('./stockfish.js', import.meta.url);
     this.worker = new Worker(url, { type: 'module' });
     this.worker.addEventListener('message', (e) => {
@@ -14,16 +19,28 @@ class EngineWorker extends EngineProcess {
         listener(e.data.message);
       }
     });
+    for (const name in this.options) {
+      this.worker.postMessage(`setoption name ${name} value ${this.options[name]}`);
+    }
   }
 
   send(message: string) {
     if (message.includes('Threads')) {
       return;
     }
-    this.worker.postMessage(message);
     for (const listener of this.listeners.stdin) {
       listener(message);
     }
+    if (message === 'stop') {
+      this.worker.terminate();
+      this.useNextWorker();
+      return;
+    }
+    const words = message.split(' ');
+    if (words[0] === 'setoption') {
+      this.options[words[2]] = words[4];
+    }
+    this.worker.postMessage(message);
   }
 }
 
