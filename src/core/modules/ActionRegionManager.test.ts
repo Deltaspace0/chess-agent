ce705e10152a3f5a5ce624d22c90398bb780eefb
Ruslan Gadeev <rgadeew@yandex.ru>
2025-10-05 14:34:14 +0300

    test: rewrite ActionRegionManager unit tests using vi.fn()

--- src/core/modules/ActionRegionManager.test.ts
+++ src/core/modules/ActionRegionManager.test.ts
@@ -1,43 +1,35 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import ActionRegionManager from './ActionRegionManager.ts';
 import { MouseMock } from './mock.ts';
 
 const mouse = new MouseMock();
+const getRegion = () => ({ left: 20, top: 20, width: 20, height: 20 });
 
 describe('Action region manager', () => {
   it('should do the action when its region is clicked', async () => {
     const actionRegionManager = new ActionRegionManager(mouse, true);
-    let value = 0;
-    actionRegionManager.addActionRegion({
-      callback: () => { value = 42 },
-      getRegion: () => ({ left: 20, top: 20, width: 20, height: 20 })
-    });
+    const callback = vi.fn();
+    actionRegionManager.addActionRegion({ callback, getRegion });
     await mouse.move({ x: 30, y: 30 });
     await mouse.click();
-    expect(value).toBe(42);
+    expect(callback).toHaveBeenCalled();
   });
 
   it('should not do the action when clicking outside the region', async () => {
     const actionRegionManager = new ActionRegionManager(mouse, true);
-    let value = 0;
-    actionRegionManager.addActionRegion({
-      callback: () => { value = 42 },
-      getRegion: () => ({ left: 20, top: 20, width: 20, height: 20 })
-    });
-    await mouse.move({ x: 50, y: 50 });
+    const callback = vi.fn();
+    actionRegionManager.addActionRegion({ callback, getRegion });
+    await mouse.move({ x: 50, y: 20 });
     await mouse.click();
-    expect(value).toBe(0);
+    expect(callback).not.toHaveBeenCalled();
   });
 
   it('should not do the action when not active', async () => {
     const actionRegionManager = new ActionRegionManager(mouse, false);
-    let value = 0;
-    actionRegionManager.addActionRegion({
-      callback: () => { value = 42 },
-      getRegion: () => ({ left: 20, top: 20, width: 20, height: 20 })
-    });
+    const callback = vi.fn();
+    actionRegionManager.addActionRegion({ callback, getRegion });
     await mouse.move({ x: 30, y: 30 });
     await mouse.click();
-    expect(value).toBe(0);
+    expect(callback).not.toHaveBeenCalled();
   });
 });
