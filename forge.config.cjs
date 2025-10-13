59a6204e7eccda2ad412d8eae6ec8fadd9d287e3
Ruslan Gadeev <rgadeew@yandex.ru>
2025-10-13 18:45:49 +0300

    build: add some files to the packager ignore list

--- forge.config.cjs
+++ forge.config.cjs
@@ -4,7 +4,11 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: './images/chess-icon'
+    icon: './images/chess-icon',
+    ignore: [
+      /^\/test_images\/.*/,
+      /^.*\.agent\.json$/
+    ]
   },
   rebuildConfig: {},
   makers: [
