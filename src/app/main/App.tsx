474228879e4f5d036bda79d7ce5605f0ce75dfe3
Ruslan Gadeev <rgadeew@yandex.ru>
2026-02-25 14:13:32 +0300

    ui: add undo move action icon

--- src/app/main/App.tsx
+++ src/app/main/App.tsx
@@ -137,7 +137,6 @@ function App() {
         <div className='flex-row'>
           <ActionButton name='bestMove' label='Best move' disabled={isNoRegion}/>
           <ActionButton name='scanMove' label='Scan move' disabled={isNoRegion}/>
-          <ActionButton name='undoMove' label='Undo move'/>
         </div>
         <div className='flex-row'>
           <ActionButton name='recognizeBoard' label='Recognize' disabled={isNoRegion}/>
@@ -220,6 +219,12 @@ function App() {
               viewBox='0 0 24 24'
               style={{ strokeWidth: 3, strokeLinecap: 'round' }}
               svgPath='M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8 M3 3v5h5'
+            />
+            <ActionIcon
+              name='undoMove'
+              viewBox='0 0 10 10'
+              style={{ strokeWidth: 2 }}
+              svgPath='M7 1L3 5L7 9'
             />
           </div>
         </div>) : chessboardComponent}
